bc843454e3a8bd5e413024546f9f9e75ba51700d
Tom <vothanh3117@gmail.com>
2023-10-24 15:46:54 -0500

    Exercise 4.1 Completed

--- BlogServer/index.js
+++ BlogServer/index.js
@@ -20,6 +20,14 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
+/*
+    GET ✅
+    POST ✅
+    DEL ✅
+    PUT ✅
+*/
+// Best to assume url is UNIQUE
+
 app.get('/api/blogs', (request, response) => {
     Blog
         .find({})
@@ -37,6 +45,41 @@ app.post('/api/blogs', (request, response) => {
             response.status(201).json(result)
         })
 })
+
+app.delete('/api/blogs/:url', (request, response, next) => {
+    const url = request.params.url
+
+    // Not going to care which one is deleted lol
+    Blog.findOneAndDelete({ "url": url })
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/blogs/:url', (request, response, next) => {
+    const url = request.params.url
+    const content = request.body
+
+    // 3rd Parameter with { new: true } is required to get modified Obj
+    Blog.findOneAndUpdate({ "url": url }, content, { new: true })
+        .then(updatedBlog => {
+            response.json(updatedBlog)
+        })
+        .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
--- README.md
+++ README.md
@@ -1,16 +1,25 @@
+<!--- Reminder that Ctrl + Shift + V to Preview Markdown in VS Code -->
 <!--- Emoji for easy copy & paste: ✅ -->
 
 # Part 4
-More backend development concepts/implementation with MongoDB
+More backend development concepts/implementation with MongoDB  
 
-### 1. Structure of backend application, introduction to testing
-    - Exercise 4.1 
+Might be better to use the REST extension than Postman -- for now  
+
+## 1. Structure of backend application, introduction to testing
+### Project Structure
+### Note on exports
+    - Exercise 4.1 ✅
     - Exercise 4.2
+Mongoose's findByIdAndRemove function for the DELETE protocol can't be used right now.
+Instead, it's better to use findOneAndDelete & findOneAndUpdate for now.
+
+### Testing Node Applications
     - Exercise 4.3
     - Exercise 4.4
     - Exercise 4.5
     - Exercise 4.6
     - Exercise 4.7
-### 2. Testing the backend
-### 3. User administration
-### 4. Token authentication
+## 2. Testing the backend
+## 3. User administration
+## 4. Token authentication
